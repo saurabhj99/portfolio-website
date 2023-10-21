7aaf2872c690afff82481617147c33d771074b37
Saurabh Joshi <me.saurabhjoshi@gmail.com>
2023-10-21 18:15:09 +0530

    added responsive navbar for smaller screen sizes | some ui fixes+

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,18 @@ import Navbar from "./components/Navbar";
 import Intro from "./components/Intro";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-
-import "./index.css"
 import Projects from "./components/Projects";
+
+import "./index.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Intro />
-      <Skills/>
-      <Projects/>
-      <Contact/>
-      <Footer />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-scroll";
 
 import "./styles/navbar.css";
+import { useState } from "react";
 
 const LINKS = [
   {
@@ -22,14 +23,50 @@ const LINKS = [
 ];
 
 const Navbar = () => {
+  // state to keep track of menu is open or not for smaller screen sizes
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <div className="portfolio-navbar">
       <div className="portfolio-navbar-lsection">
         <div className="portfolio-navbar-logo">My Portfolio</div>
       </div>
-      <div className="portfolio-navbar-rsection">
+      <div
+        className="portfolio-navbar-menu-toggler"
+        onClick={() => setIsMenuOpen(true)}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          x="0px"
+          y="0px"
+          width="40"
+          height="40"
+          viewBox="0 0 50 50"
+        >
+          <path d="M 0 7.5 L 0 12.5 L 50 12.5 L 50 7.5 Z M 0 22.5 L 0 27.5 L 50 27.5 L 50 22.5 Z M 0 37.5 L 0 42.5 L 50 42.5 L 50 37.5 Z"></path>
+        </svg>
+      </div>
+      <div
+        className={`portfolio-navbar-rsection ${
+          isMenuOpen ? "menu-isOpen" : ""
+        }`}
+      >
+        <div className="portfolio-navbar-menu-close">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="40"
+            height="40"
+            viewBox="0 0 50 50"
+            onClick={() => isMenuOpen ? setIsMenuOpen(false) : null}
+          >
+            <path d="M 7.71875 6.28125 L 6.28125 7.71875 L 23.5625 25 L 6.28125 42.28125 L 7.71875 43.71875 L 25 26.4375 L 42.28125 43.71875 L 43.71875 42.28125 L 26.4375 25 L 43.71875 7.71875 L 42.28125 6.28125 L 25 23.5625 Z"></path>
+          </svg>
+        </div>
         {LINKS.map(({ title, link }) => (
           <Link
+            onClick={() => isMenuOpen ? setIsMenuOpen(false): null}
             key={`key-${title}`}
             className="portfolio-navbar-link"
             to={link}
